3fc89951398e619e69ca481d169d775dbc405561
elpierZON <elpierZONRemasterizado@gmail.com>
2024-09-26 09:59:12 -0500

    nuevos ejercicios en la semana 7

--- Semana 7/s7.py
+++ Semana 7/s7.py
@@ -13,7 +13,64 @@ print(subcadena3)
 # --------------Listas---------------
 #Escribir un programa que imprima una lista acompañado de su indice, que tome valores desde o hasta n-1
 
-
+print ("Lista de numeros")
 lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-for indice, valor in enumerate(lista):
-    print(f"{indice} - {valor}")
+for i, l in enumerate(lista):
+    print(i , l)
+
+#lista iterada
+print("------------Lista de iterada---------")
+lista1 = [20, 18, 19, 20]
+lista2 = ["Juan","Martin","Jose","Pepe", ]
+
+for l1,l2 in zip (lista,lista2): #zip sirve para unir 2 o mas listas
+    print(l1,l2)
+
+#Escribir un programa que permita iterar iterar las listas usando los índices como hemos visto al principio, y 
+# haciendo uso de Len(), que nos devuelva la longitud de la lista 
+
+print ("Iterando listas con longitud ")
+lista4 = ["Juan","Martin","Jose","Pepe","Tito" ]
+
+len_lista4 = len(lista4)
+
+for i,l in enumerate(lista4):
+    print(i,l,len_lista4)
+
+#EJERCICIO 02:
+#Escribir una función que reciba una lista de notas y devuelva la lista de calificaciones correspondientes a esas notas.
+
+def notas_a_calificaciones(notas):
+    calificaciones = []
+    for i in notas:
+        if 18 <= i <= 20:
+            calificacion = "Excelente"
+        elif 14 <= i < 18:
+            calificacion = "Bueno"
+        elif 10 <= i < 14:
+            calificacion = "Suficiente"
+        else:
+            calificacion = "Insuficiente"
+
+        calificaciones.append(calificacion)
+
+    return calificaciones
+notas = [20, 18, 13, 10, 7]
+resultado = notas_a_calificaciones(notas)
+print(resultado)  
+
+
+# Escribir una función que simule una calculadora científica que permita calcular el seno, coseno, tangente, exponencial y logaritmo neperiano. 
+# La función preguntará al usuario el valor y la función a aplicar, y mostrará por pantalla una tabla con los enteros de 1 al valor introducido 
+# y el resultado de aplicar la función a esos enteros.
+
+print("CALCULADORA CIENTIFICA")
+
+def calculadora():
+    print("Introduzca la operacion")
+    valor = int(input())
+    print("Intruduzca")
+    operacion = input("Elija la operacion que desea: ")
+    
+
+
